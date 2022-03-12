e19efde6ae81d69c45ce3bed28b292996638de35
Kostya <tsyvilko12@gmail.com>
2022-03-12 15:47:47 +0300

    Initial Commit

---
+++ PaintIOS/PaintIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintIOSApp.swift
+//  PaintIOS
+//
+//  Created by Kostya Tsyvilko on 12.03.22.
+//
+
+import SwiftUI
+
+@main
+struct PaintIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
